3d2bccb93c17815f8853f17ac458573f6b222984
DipakKumarChaudhary <np03cs4s210291@heraldcollege.edu.np>
2023-05-22 22:13:16 +0545

    First Commit

---
+++ signup.php
@@ -0,0 +1,260 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if(isset($_POST['signup']))
+{
+$count_my_page = ("studentid.txt");
+$hits = file($count_my_page);
+$hits[0] ++;
+$fp = fopen($count_my_page , "w");
+fputs($fp , "$hits[0]");
+fclose($fp); 
+$StudentId= $hits[0];   
+$fname=$_POST['fullanme'];
+$mobileno=$_POST['mobileno'];
+$email=$_POST['email']; 
+$password=md5($_POST['password']); 
+$status=1;
+$sql="INSERT INTO  tblstudents(StudentId,FullName,MobileNumber,EmailId,Password,Status) VALUES(:StudentId,:fname,:mobileno,:email,:password,:status)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':StudentId',$StudentId,PDO::PARAM_STR);
+$query->bindParam(':fname',$fname,PDO::PARAM_STR);
+$query->bindParam(':mobileno',$mobileno,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':password',$password,PDO::PARAM_STR);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+echo '<script>alert("Your Registration successfull and your student id is  "+"'.$StudentId.'")</script>';
+}
+else 
+{
+echo "<script>alert('Something went wrong. Please try again');</script>";
+}
+}
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Online Library Management System | Student Signup</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+<script type="text/javascript">
+function valid()
+{
+if(document.signup.password.value!= document.signup.confirmpassword.value)
+{
+alert("Password and Confirm Password Field do not match  !!");
+document.signup.confirmpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+<script>
+function checkAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'emailid='+$("#emailid").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>    
+
+</head>
+<body>
+    <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line">User Signup</h4>
+                
+                            </div>
+
+        </div>
+             <div class="row">
+           
+<div class="col-md-9 col-md-offset-1">
+               <div class="panel panel-danger">
+                        <div class="panel-heading">
+                           SINGUP FORM
+                        </div>
+                        <div class="panel-body">
+                            <form name="signup" method="post" onSubmit="return valid();">
+<div class="form-group">
+<label>Enter Full Name</label>
+<input class="form-control" type="text" name="fullanme" autocomplete="off" required />
+</div>
+
+
+<div class="form-group">
+<label>Mobile Number :</label>
+<input class="form-control" type="text" name="mobileno" maxlength="10" autocomplete="off" required />
+</div>
+                                        
+<div class="form-group">
+<label>Enter Email</label>
+<input class="form-control" type="email" name="email" id="emailid" onBlur="checkAvailability()"  autocomplete="off" required  />
+   <span id="user-availability-status" style="font-size:12px;"></span> 
+</div>
+
+<!-- <div class="form-group">
+  <label for="password">Password</label>
+  <div class="password-toggle">
+    <input class="form-control" type="password" id="password" name="password" autocomplete="off" required>
+    <span class="toggle-icon"><i class="fa fa-eye-slash"></i></span>
+  </div>
+</div>
+
+<div class="form-group">
+  <label for="confirm-password">Confirm Password</label>
+  <div class="password-toggle">
+    <input class="form-control" type="password" id="confirm-password" name="confirm-password" autocomplete="off" required>
+    <span class="toggle-icon"><i class="fa fa-eye-slash"></i></span>
+  </div>
+</div> -->
+<div class="form-group">
+<label>Enter Password</label>
+<input class="form-control" type="password" id="password" name="password" autocomplete="off" required  />
+<i class="bi bi-eye-slash" id="togglePassword"></i>
+</div>
+
+<div class="form-group">
+<label>Confirm Password </label>
+<input class="form-control"  type="password" name="confirmpassword" autocomplete="off" required  />
+</div>
+
+
+<button type="submit" name="signup" class="btn btn-danger" id="submit">Register Now </button>
+
+<!-- <script>
+  const togglePassword = document.querySelectorAll('.toggle-icon');
+  const passwordFields = document.querySelectorAll('input[type="password"]');
+  const registerButton = document.querySelector('#register-button');
+
+  togglePassword.forEach((toggle, index) => {
+    toggle.addEventListener('click', function (e) {
+      const icon = this.querySelector('i');
+      const passwordField = passwordFields[index];
+
+      if (passwordField.type === 'password') {
+        passwordField.type = 'text';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+      } else {
+        passwordField.type = 'password';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+      }
+    });
+  });
+
+  const validatePasswords = () => {
+    const password = passwordFields[0].value;
+    const confirmPassword = passwordFields[1].value;
+
+    if (password === confirmPassword) {
+      registerButton.disabled = false;
+    } else {
+      registerButton.disabled = true;
+    }
+  };
+
+  passwordFields.forEach((field) => {
+    field.addEventListener('input', validatePasswords);
+  });
+
+  registerButton.addEventListener('click', () => {
+    const password = passwordFields[0].value;
+    const confirmPassword = passwordFields[1].value;
+
+    if (password === confirmPassword) {
+      // Replace the following code with your actual registration logic
+      const registrationData = {
+        password: password
+      };
+
+      // Make an AJAX request to your registration endpoint
+      // Example using the Fetch API
+      fetch('/your-registration-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(registrationData)
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Handle the response from the server
+          if (data.success) {
+            // Successful registration
+            console.log('Registered successfully!');
+          } else {
+            // Failed registration
+            console.log('Registration failed. Please try again.');
+          }
+        })
+        .catch(error => {
+          console.log('An error occurred during registration:', error);
+        });
+    }
+  });
+</script> -->
+
+
+
+
+<!-- <div class="form-group">
+<label>Enter Password</label>
+<input class="form-control" type="password" name="password" autocomplete="off" required  />
+</div>
+
+<div class="form-group">
+<label>Confirm Password </label>
+<input class="form-control"  type="password" name="confirmpassword" autocomplete="off" required  />
+</div> -->
+
+
+                                    </form>
+                            </div>
+                        </div>
+                            </div>
+        </div>
+    </div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+     <?php include('includes/footer.php');?>
+     <!-- Footer SECTION END-->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
---
+++ wlcm.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if($_SESSION['alogin']!=''){
+$_SESSION['alogin']='';
+}
+if(isset($_POST['login']))
+{
+$username=$_POST['username'];
+$password=md5($_POST['password']);
+$sql ="SELECT UserName,Password FROM admin WHERE UserName=:username and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':username', $username, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location ='admin/dashboard.php'; </script>";
+} else{
+echo "<script>alert('Invalid Details');</script>";
+}
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Online Library Management System</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+    <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->  
+<div class="content-wrapp">
+<div class="content-items">
+    <img src="assets\img\7.jpg" alt="">
+    <div class="text">
+        <br>
+        <h1 style="text-align: center; line-height:60px; font-size: 35px; font-weight: bold;">Welcome to Our Online library  Management System</h1><br>
+		<h1 style="text-align: center;font-size: 25px; font-weight: bold;">"We stand behind Our Success"</h1><br>
+		<!-- <h1 style="text-align: center;font-size: 25px;">Closes at: 15:00 </h1><br> -->
+        <a href="signup.php"><button type="submit" name="User Reg" class="btnreg">User Registration </button></a>
+        <a href="index.php"><button type="submit" name="User login" class="btn">User login </button></a>
+    
+	</div>
+</div>
+
+</div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+ <?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</script>
+</body>
+</html>
